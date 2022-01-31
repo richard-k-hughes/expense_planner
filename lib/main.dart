b9690804f72f1096b20546503f17ea6263f2c3c7
Richard <grnganon@gmail.com>
2022-01-30 22:50:54 -0500

    Starting baseline for calander chart and transaction list

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,27 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        // Column takes the width of the widest child
+        children: <Widget>[
+          // Card takes the childs width unless there is a parent with a clearly defined width. See the two cards below!
+          Card(
+            // Card will takes the width of the child
+            child: Container(
+              child: Text('Chart of Dates'),
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+            elevation: 5,
+          ),
+          Container(
+            // width: double.infinity,
+            child: Card(
+              // Card also takes the width of the parent
+              child: Text('Transaction List'),
+            ),
+          )
+        ],
       ),
     );
   }
