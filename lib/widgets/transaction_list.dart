2e8c68171389bbdc481afb97de9de4682c74e1d9
Richard <grnganon@gmail.com>
2022-03-20 21:39:38 -0400

    Using listview and builder for more performant lazy loaded lists

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,48 +11,49 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions
-              .map((transaction) => Card(
-                      child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transaction.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transaction.title,
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            // Can also just pass the format string into DateFormat like 'MMM dd, yyyy'
-                            DateFormat().add_yMMMd().format(transaction.date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  )))
-              .toList(),
-        ),
+      // A list view is a built in column + singleChildScrollView widget
+      // List view must have a height provided, unlike Column it isnt just max screen height it can get
+      // List view can just be used with children and you widgets but in the case of a long or unknown list width it can be non-performant
+      // Listview.builder will lazy load lists and should be used instead if we have long lists!
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.purple,
+                  width: 2,
+                )),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$${transactions[index].amount}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transactions[index].title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    // Can also just pass the format string into DateFormat like 'MMM dd, yyyy'
+                    DateFormat().add_yMMMd().format(transactions[index].date),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ));
+        },
+        itemCount: transactions.length,
       ),
     );
   }
