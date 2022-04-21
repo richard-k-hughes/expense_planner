af9af46bdd24ad6102a08af3d1654d0800243bd6
Richard <grnganon@gmail.com>
2022-04-20 22:56:32 -0400

    Added no transactions view

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(id: '1', title: 'Boba', amount: 7.25, date: DateTime.now()),
-    Transaction(
-        id: '2', title: 'Chichen Nuggets', amount: 6.99, date: DateTime.now())
+    // Transaction(id: '1', title: 'Boba', amount: 7.25, date: DateTime.now()),
+    // Transaction(
+    //     id: '2', title: 'Chichen Nuggets', amount: 6.99, date: DateTime.now())
   ];
 
   void _addNewTransaction(String title, double amount) {
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,46 +15,69 @@ class TransactionList extends StatelessWidget {
       // List view must have a height provided, unlike Column it isnt just max screen height it can get
       // List view can just be used with children and you widgets but in the case of a long or unknown list width it can be non-performant
       // Listview.builder will lazy load lists and should be used instead if we have long lists!
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-              child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Theme.of(context).primaryColor,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${transactions[index].amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No Transaction added yet!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    transactions[index].title,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 300,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  Text(
-                    // Can also just pass the format string into DateFormat like 'MMM dd, yyyy'
-                    DateFormat().add_yMMMd().format(transactions[index].date),
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              )
-            ],
-          ));
-        },
-        itemCount: transactions.length,
-      ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${transactions[index].amount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          transactions[index].title,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          // Can also just pass the format string into DateFormat like 'MMM dd, yyyy'
+                          DateFormat()
+                              .add_yMMMd()
+                              .format(transactions[index].date),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ));
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
